69475f491d605f2e1f07fb204f2aa2a3008ed19b
Brooke Osborn <dsullivan7@hotmail.com>
2014-01-11 17:33:06 -0500

    making different structure

---
+++ public/main.js
@@ -0,0 +1,20 @@
+var DannySite = DannySite || (function () {
+    var aboutHtml = "<h1 id='about'>I program things for money</h1> <a target='_blank' id='resume_link' href='https://docs.google.com/document/d/15Z2Cl8cjrDEfFR8IfOK3h01BmLCfTv0bo7zUYR_7I50/edit?pli=1'>Resume</a>";
+
+    var linksHtml = "<ul class='project_list'><li><a target='_blank' href='https://github.com/dsullivan7/monty-hall'>Monty Hall</a></li><li><a target='_blank' href='https://github.com/dsullivan7/tictac'>Tic Tac Toe</a></li></ul>";
+
+    var populateHtml = function(tab){
+        switch(tab){
+            case "about":
+                $("#page_body").html(aboutHtml);
+                break;
+            case "links":
+                $("#page_body").html(linksHtml);
+                break;
+        }
+    };
+    
+    return {
+        populateHtml:populateHtml
+    };
+})();
