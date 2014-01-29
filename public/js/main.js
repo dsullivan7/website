34601dde22707921bb52d0a21544c55bd1ab9cf4
Brooke Osborn <dsullivan7@hotmail.com>
2014-01-29 18:30:46 -0500

    adding new project

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,11 @@
 var DannySite = DannySite || (function () {
     var aboutHtml = "<h1>You can read more about me <a target='_blank' id='resume_link' href='https://docs.google.com/document/d/15Z2Cl8cjrDEfFR8IfOK3h01BmLCfTv0bo7zUYR_7I50/edit?pli=1'>Here</a></h1>";
 
-    var linksHtml = "<h1>Here are some projects I've done</h1><ul class='project_list'><li><a target='_blank' href='https://github.com/dsullivan7/monty-hall'>Monty Hall</a></li><li><a target='_blank' href='https://github.com/dsullivan7/tictac'>Tic Tac Toe</a></li></ul>";
+    var linksHtml = "<h1>Here are some projects I've done</h1><ul class='project_list'>\
+<li><a target='_blank' href='https://github.com/dsullivan7/monty-hall'>Monty Hall</a></li>\
+<li><a target='_blank' href='https://github.com/dsullivan7/tictac'>Tic Tac Toe</a></li>\
+<li><a target='_blank' href='https://github.com/dsullivan7/stringsim'>String Simularity</a></li>\
+</ul>";
 
     var populateHtml = function(tab){
         switch(tab){
