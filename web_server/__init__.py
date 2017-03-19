9ead2ca6752c4999634cd701142034d8297a15d3
dsullivan7 <dbsullivan23@gmail.com>
2017-03-19 15:33:14 +1100

    changing from ruby to python

---
+++ web_server/__init__.py
@@ -0,0 +1,10 @@
+"""Initialize app."""
+from flask import Flask
+
+from werkzeug.contrib.fixers import ProxyFix
+
+app = Flask(__name__, static_url_path='')
+app.wsgi_app = ProxyFix(app.wsgi_app)
+
+
+import web_server.views
