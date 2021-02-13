bcee2e6294fe77cdb5172dc677c3014914370b5d
danny <danny@perxhealth.com>
2021-02-13 14:51:45 -0500

    adding icons

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Button from 'react-bootstrap/Button'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
 
 import './App.css'
+
+library.add(fab)
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Danny Sullivan
-        </p>
-      </header>
-      <div>
-        <p>Hello!</p>
+      <div className="header d-flex text-center align-items-center w-100 p-4 justify-content-center">
+        <span className="px-2 header-text">Danny Sullivan</span>
+      </div>
+      <div className="d-flex content justify-content-around p-4">
+        <Button className="icon-button" href="https://stackoverflow.com/users/2246559/danny-sullivan">
+          <FontAwesomeIcon size="3x" icon={['fab', 'stack-overflow']} />
+        </Button>
+        <Button className="icon-button" href="https://stackoverflow.com/users/2246559/danny-sullivan">
+          <FontAwesomeIcon size="3x" icon={['fab', 'linkedin']} />
+        </Button>
+        <Button className="icon-button" href="https://www.linkedin.com/in/danny-sullivan-8a843846/">
+          <FontAwesomeIcon size="3x" icon={['fab', 'github']} />
+        </Button>
       </div>
     </div>
   )
